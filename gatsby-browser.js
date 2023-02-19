f5bdef9fd9ba771b735fee2cc4b6bd5602a0e686
Steven Quinn <stevenquinn@me.com>
2023-02-18 16:14:21 -0800

    First commit off starter template

---
+++ gatsby-browser.js
@@ -0,0 +1,7 @@
+// custom typefaces
+import "@fontsource/montserrat/variable.css"
+import "@fontsource/merriweather"
+// custom CSS styles
+import "./src/style.css"
+// Highlighting for code blocks
+import "prismjs/themes/prism.css"
